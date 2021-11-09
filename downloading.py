8abd4a425858dd1c2b2effe94305bc12c197a4cd
F1NH4WK <araujoplayer@gmail.com>
2021-11-09 20:44:12 -0300

    Documentanting somethings

--- downloading.py
+++ downloading.py
@@ -5,5 +5,3 @@ def download(url: str, name: str):
     print(yt.streams.filter(mime_type= 'audio/webm', abr= '160kbps'))
     stream = yt.streams.get_by_itag('251')
     stream.download(filename= f'{name}.webm', timeout= None)
-
-download('https://www.youtube.com/watch?v=5uV4IMkokGk')
--- gettingytvideo.py
+++ gettingytvideo.py
@@ -3,17 +3,25 @@ from time import sleep
 from selenium.webdriver.common.keys import Keys
 import os
 
+
+# DEFINING THE BROWSER AND GOING TO YOUTUBE
 driver = webdriver.Chrome()
 driver.get("https://www.youtube.com")
 
+
+# FINDING THE SEARCH INPUT BY XPATH AND SEARCHING.
 element = driver.find_element_by_xpath("/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input")
 element.send_keys("Runaway")
+
+# USING SLEEP TO NOT END THE PROGRAM BY TIMEOUT
 sleep(3)
 element.send_keys(Keys.ENTER)
 sleep(3)
 
+#GETTING FIRST FIVE OCURRENCES.
 for i in range(1, 6):
     video = driver.find_element_by_xpath(f'/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[{i}]/div[1]/div/div[1]/div/h3/a')
     if i == 1: os.system('cls' if os.name == 'nt' else 'clear')
     print(f'Video: \033[1;34m{video.get_attribute("title")}\033[m')
 
+    
