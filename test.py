3621fd88ef01d7fe8d9a8555aebfd3d20965df9a
F1NH4WK <araujoplayer@gmail.com>
2021-11-09 20:27:36 -0300

    First Commit

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import pytube as py
 
-
-def download(url: str):
-    yt = py.YouTube('https://www.youtube.com/watch?v=5uV4IMkokGk')
+def download(url: str, name: str):
+    yt = py.YouTube(url)
     print(yt.streams.filter(mime_type= 'audio/webm', abr= '160kbps'))
     stream = yt.streams.get_by_itag('251')
-    stream.download(filename= 'audio.webm', timeout= None)
+    stream.download(filename= f'{name}.webm', timeout= None)
+
+download('https://www.youtube.com/watch?v=5uV4IMkokGk')
