52857a69c95ce1b33cd7171a2faf826b6e88c185
F1NH4WK <araujoplayer@gmail.com>
2021-11-09 21:14:52 -0300

    Prototype working

--- gettingytvideo.py
+++ gettingytvideo.py
@@ -1,17 +1,19 @@
 from selenium import webdriver
 from time import sleep
+import selenium
 from selenium.webdriver.common.keys import Keys
 import os
+from downloading import download
 
+music_name = input('Music name: ')
 
 # DEFINING THE BROWSER AND GOING TO YOUTUBE
 driver = webdriver.Chrome()
 driver.get("https://www.youtube.com")
 
-
 # FINDING THE SEARCH INPUT BY XPATH AND SEARCHING.
 element = driver.find_element_by_xpath("/html/body/ytd-app/div/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input")
-element.send_keys("Runaway")
+element.send_keys(music_name)
 
 # USING SLEEP TO NOT END THE PROGRAM BY TIMEOUT
 sleep(3)
@@ -22,6 +24,9 @@ sleep(3)
 for i in range(1, 6):
     video = driver.find_element_by_xpath(f'/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[{i}]/div[1]/div/div[1]/div/h3/a')
     if i == 1: os.system('cls' if os.name == 'nt' else 'clear')
-    print(f'Video: \033[1;34m{video.get_attribute("title")}\033[m')
+    print(f'[\033[1;32m{i}\033[m] Video: \033[1;34m{video.get_attribute("title")}\033[m')
 
-    
+chose = int(input("Chose one of them: "))
+video_chosed = driver.find_element_by_xpath(f'/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[{chose}]/div[1]/div/div[1]/div/h3/a')
+download(video_chosed.get_attribute('href'), input("Name of archive: "))
+driver.close()
