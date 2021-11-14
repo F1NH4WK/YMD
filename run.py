08cb9aa91eb476279d1583e43f061ce8131cb06e
F1NH4WK <araujoplayer@gmail.com>
2021-11-14 01:46:56 -0300

    Improving and documentation somethings

--- run.py
+++ run.py
@@ -10,15 +10,15 @@ chose = 0
 driver = webdriver.Chrome()
 driver.get(f"https://www.youtube.com/results?search_query={music_name}")
 
-#GETTING FIRST FIVE OCURRENCES.
+# GETTING FIRST THE FIVE OCURRENCES.
 while not chose in range(1,6):
     for i in range(1, 6):
         video = driver.find_element_by_xpath(f'/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[{i}]/div[1]/div/div[1]/div/h3/a')
         if i == 1: os.system('cls' if os.name == 'nt' else 'clear')
-        print(f'[\033[1;32m{i}\033[m] Video: \033[1;34m{video.get_attribute("title")}\033[m')
-    chose = int(input("Chose one of them: "))
+        print(f'[\033[1;32m{i}\033[m] \033[1;34m{video.get_attribute("title")}\033[m')
+    chose = int(input('Chose one of them: '))
 
+# CHOSING THE YT VIDEO AND GET ITS LINK, THEN DOWNLOADING IT WITH PYTUBE.
 video_chosed = driver.find_element_by_xpath(f'/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[{chose}]/div[1]/div/div[1]/div/h3/a')
 download(video_chosed.get_attribute('href'), video_chosed.get_attribute('title'))
-
 driver.close()
